4e679bb2429fa288fb7e7a0d14e436941b1473ec
sr-vinnamuri <115513028+sr-vinnamuri@users.noreply.github.com>
2022-10-24 16:54:10 -0400

    initial commit

---
+++ src/main/java/com/learning/automation/javalarning/Firstprogram.java
@@ -0,0 +1,15 @@
+package com.learning.automation.javalarning;
+
+public class Firstprogram {
+
+	public static void main(String[] args)
+
+	{
+
+		System.out.println("SUDHARANI,");
+		System.out.println("748,SUMMERWALK DRIVE,");
+		System.out.println("GAITHERSBURG,");
+		System.out.println("MARYLAND - 20878");
+
+	}
+}
---
+++ src/main/java/com/learning/automation/javalarning/LearningLoops.java
@@ -0,0 +1,16 @@
+package com.learning.automation.javalarning;
+
+public class LearningLoops {
+	
+	
+	public static void main(String[] args)
+
+	{
+
+		int n [] = {1, 2, 3, 4, 5};
+		System.out.println(n.length);
+		
+		
+	}
+}
+
